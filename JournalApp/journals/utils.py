620cdc0595008dfec09e9f8f7a0a151d0e1b65da
hamna-akhtar <hamna.akhtar@ignicube.com>
2025-09-22 15:52:38 +0500

    change model for AI flow

--- JournalApp/journals/utils.py
+++ JournalApp/journals/utils.py
@@ -4,22 +4,26 @@ import ast
 
 def extract_action_items(journal_text):
     llm = Llama(
-        model_path="./llm_models/phi-2.Q4_K_M.gguf",
+        model_path="./llm_models/Phi-3-mini-4k-instruct-q4.gguf",
         n_ctx=2048,
         n_threads=8
     )
 
     prompt = f""" 
             Extract actionable items (tasks to be done) from the given journal entry. 
-            List only all the specific tasks, calls, appointments, or commitments etc that need to be done.
+            Precisely list only all those specific tasks, calls, appointments, or commitments etc that are mentioned in the text, to be done in future.
             Here's the journal entry:
             {journal_text}
 
-            Actionable items (as a python list of strings):
+            Actionable items (your response should be formatted as a single python list of strings):
             """
 
     response = llm(prompt, max_tokens=150, temperature=0.7)
-    res = response["choices"][0]["text"].strip()
-    # print("RESPONSE: ", res)
-    return ast.literal_eval(re.findall(r'\[.*?]', res)[0])
+    response_text = response["choices"][0]["text"].strip()
+    # find python list of strings from response
+    result_lists = re.findall(r"\[[^\[\]]*['\"][^\[\]]*['\"][^\[\]]*]", response_text)
+    if len(result_lists)>0:
+        return ast.literal_eval(result_lists[0])
+    else:
+        return []
 
--- JournalApp/journals/views.py
+++ JournalApp/journals/views.py
@@ -37,7 +37,7 @@ class JournalEntryListCreateView(generics.ListCreateAPIView):
 
         new_tasks = extract_action_items(journal.content)
         for task_desc in new_tasks:
-            if not Task.objects.filter(created_by=self.request.user, description=task_desc.strip()).exists():
+            if not (Task.objects.filter(created_by=self.request.user, description=task_desc.strip()).exists() or task_desc == 'None'):
                 Task.objects.create(created_by=self.request.user, description=task_desc.strip())
 
 
@@ -55,7 +55,7 @@ class JournalEntryDetailView(generics.RetrieveUpdateDestroyAPIView):
 
         new_tasks = extract_action_items(journal.content)
         for task_desc in new_tasks:
-            if not Task.objects.filter(created_by=self.request.user, description=task_desc.strip()).exists():
+            if not (Task.objects.filter(created_by=self.request.user, description=task_desc.strip()).exists() or task_desc == 'None'):
                 Task.objects.create(created_by=self.request.user, description=task_desc.strip())
 
 
